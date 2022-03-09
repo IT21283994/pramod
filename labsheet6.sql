892af0e9f68ba69393b040da4fca31109d520aa1
IT21283994 <99554071+IT21283994@users.noreply.github.com>
2022-03-09 15:33:45 +0530

    Update labsheet6.sql

--- labsheet6.sql
+++ labsheet6.sql
@@ -21,3 +21,30 @@ where C.C_fee > 100000 AND C.CID = S.CID
 select M.Mname
 from Module M, Offers O
 where O.Accademic_year = 'Y1' AND M.Mcode = O.Mcode AND O.CID= 'IT'
+
+
+
+LAB 7
+
+select C.CID, count(S.SID) AS 'No of students'
+From Course C, Student S
+where C.CID = S.CID
+Group by C.CID 
+
+select C.Cname , count(S.SID) AS 'No of students'
+From Course C, Student S
+where C.CID = S.CID
+Group by C.Cname, C.CID
+
+select C.Cname, count(O.Mcode)
+from Course C, Offers O
+where C.CID= O.CID AND O.Accademic_year ='y1'
+Group by C.CID, C.Cname
+having count(O.Mcode)>2;
+
+select C.Cname,O.Accademic_year, count(O.Mcode)
+from Course C, Offers O
+where C.CID= O.CID 
+Group by C.CID, C.Cname,O.Accademic_year
+having count(O.Mcode)>2
+order by count(O.Mcode);
